2152abc53aff24bf80a09fe885216f443ab2539c
Lu Dai <106527173+stellaludai@users.noreply.github.com>
2025-05-28 15:47:55 +0800

    Update Markdown.vue

--- src/components/sections/Markdown.vue
+++ src/components/sections/Markdown.vue
@@ -1,6 +1,5 @@
 <script setup>
 import page from '../mds/md.mdx';
-import pipelinepage from '../mds/pipelinemd.mdx';
 </script>
 
 <template>
@@ -10,14 +9,6 @@ import pipelinepage from '../mds/pipelinemd.mdx';
     <el-row justify="center">
         <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
             <page />
-        </el-col>
-    </el-row>
-
-    <el-divider />
-    
-      <el-row justify="center">
-        <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
-            <pipelinepage />
         </el-col>
     </el-row>
 
