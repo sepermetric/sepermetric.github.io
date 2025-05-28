a5dc35496e656dc7aed295e0f5362483e0004aa1
Lu Dai <106527173+stellaludai@users.noreply.github.com>
2025-05-28 15:46:01 +0800

    Create PipelineMarkdown.vue

---
+++ src/components/sections/PipelineMarkdown.vue
@@ -0,0 +1,16 @@
+<script setup>
+import pipelinepage from '../mds/pipelinemd.mdx';
+</script>
+
+<template>
+  <div>
+    <el-divider />
+    
+      <el-row justify="center">
+        <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="12">
+            <pipelinepage />
+        </el-col>
+    </el-row>
+
+  </div>
+</template>
